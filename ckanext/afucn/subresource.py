9a54cfd79eeb1def243beed1f7c76ace127be9a1
blagoja <blagojabozinovski@gmail.com>
2025-02-03 16:37:25 +0100

    Test add resource action

--- ckanext/afucn/subresource.py
+++ ckanext/afucn/subresource.py
@@ -4,13 +4,18 @@ from ckan.common import g
 
 def create_subresource(context, resource_dict):
 
-    x = tk.get_action("status_show")(context)
+    local_file_path = '/home/blagoja/Downloads/sample_data/sample.txt'
+    
+    with open(local_file_path, 'rb') as file_content:
+        data_dict = {
+            'package_id': resource_dict['package_id'],
+            'name': 'Subresource',
+            'upload': file_content,
+        }
+
+    # x = tk.get_action("resource_create")(context, data_dict)
+
     print("======================================================================")
     print(g.user)
     print("======================================================================")
-    print(resource_dict)
-    print("======================================================================")
-    print(x)
-    print("======================================================================")
-
-    return
+    return 
