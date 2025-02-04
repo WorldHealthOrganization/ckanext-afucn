e33ecd7a8fc43c3c1eb7f594da69a641d72c8e33
blagoja <blagojabozinovski@gmail.com>
2025-02-04 22:58:57 +0100

    Add subresource based on resource format

--- ckanext/afucn/subresource.py
+++ ckanext/afucn/subresource.py
@@ -1,21 +1,42 @@
 import ckan.plugins.toolkit as tk
 from ckan.common import g
+from ckan.common import config
+import os
+import logging
+from pathlib import Path
+from shutil import copyfile
+
+log = logging.getLogger(__name__)
+
+try:
+    storage_path = config.get('ckan.storage_path')
+except:
+    log.critical('''Please specify a ckan.storage_path in your config
+                         for your uploads''')
 
 
 def create_subresource(context, resource_dict):
 
-    local_file_path = '/home/blagoja/Downloads/sample_data/sample.txt'
+    if resource_dict['format'] == 'CSV':
     
-    with open(local_file_path, 'rb') as file_content:
         data_dict = {
             'package_id': resource_dict['package_id'],
-            'name': 'Subresource',
-            'upload': file_content,
+            'name': 'test.jpg',
+            'url': 'test.jpg',
+            'url_type': 'upload',
         }
 
-    # x = tk.get_action("resource_create")(context, data_dict)
+        x = tk.get_action("resource_create")(context, data_dict)
+        upload_path = storage_path + '/resources/' + x['id'][0:3] + "/" + x['id'][3:6]
+        upload_filename = x['id'][6:]
+        filepath = Path(os.path.join(upload_path, upload_filename))
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+        copyfile('/home/blagoja/Downloads/demo/test.jpeg', filepath)
+        return
 
-    print("======================================================================")
-    print(g.user)
-    print("======================================================================")
-    return 
+    else:
+        print("======================================================================")
+        print(g.user)
+        print("======================================================================")
+        return
+
