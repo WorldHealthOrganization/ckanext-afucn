ba84fd32387e3eba0ef93271d7f5a28b54b2f559
ainoman11 <nomana@who.int>
2025-03-12 00:16:27 +0100

    cleaned some unneeded files. especially javascript

--- ckanext/afucn/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// assets/js/main.js
-
-import './vendor';        // Import vendor files (third-party libraries)
-import '../css/styles.css';  // Import your custom CSS
-import '../css/custom.css';  // Import your custom CSS
-import '../css/afucn.css';  // Import your custom CSS
-//import '../sass/styles.scss'; // Import your custom Sass (if needed)
-
-// Your custom JavaScript code here
