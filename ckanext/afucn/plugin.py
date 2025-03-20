460b3421748c64a0e21bc2fc846b60ae44ec3f7a
blagoja <blagojabozinovski@gmail.com>
2025-03-20 11:35:31 +0100

    Remove group from facets

--- ckanext/afucn/plugin.py
+++ ckanext/afucn/plugin.py
@@ -38,7 +38,6 @@ class AfucnPlugin(plugins.SingletonPlugin, DefaultTranslation):
     def dataset_facets(self, facets_dict, package_type):
         facets_dict['programmes'] = toolkit._('Programmes')
         facets_dict['countries'] = toolkit._('Countries')
-        facets_dict['groups'] = toolkit._('Groups')
         facets_dict['tags'] = toolkit._('Tags')
         facets_dict['res_format'] = plugins.toolkit._('Format')
         return facets_dict
@@ -46,7 +45,6 @@ class AfucnPlugin(plugins.SingletonPlugin, DefaultTranslation):
     def group_facets(self, facets_dict, group_type, package_type):
         facets_dict['programmes'] = toolkit._('Programmes')
         facets_dict['countries'] = toolkit._('Countries')
-        facets_dict['groups'] = toolkit._('Groups')
         facets_dict['tags'] = toolkit._('Tags')
         facets_dict['res_format'] = plugins.toolkit._('Format')
         return facets_dict
@@ -54,7 +52,6 @@ class AfucnPlugin(plugins.SingletonPlugin, DefaultTranslation):
     def organization_facets(self, facets_dict, organization_type, package_type):
         facets_dict['programmes'] = toolkit._('Programmes')
         facets_dict['countries'] = toolkit._('Countries')
-        facets_dict['groups'] = toolkit._('Groups')
         facets_dict['tags'] = toolkit._('Tags')
         facets_dict['res_format'] = plugins.toolkit._('Format')
         return facets_dict
